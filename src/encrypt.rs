b229854b16e6a8767928f03843a96bef10cbb103
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-02-02 14:03:41 +0100

    better edit help message
    and better name in the usage

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -5,7 +5,7 @@ use crate::error::Result;
 use crate::util::{encrypt_yaml, load_recipients, stdin_or_file, stdout_or_file};
 
 pub fn encrypt(args: &EncryptArgs) -> Result<()> {
-    let recipients = load_recipients(&args.recipients, &args.recipients_files)?;
+    let recipients = load_recipients(&args.recipients, &args.recipient_files)?;
     debug!("loading yaml file: {:?}", args.file);
     let input_data: sy::Value = sy::from_reader(stdin_or_file(&args.file)?)?;
     let output_data = encrypt_yaml(&input_data, &recipients)?;
