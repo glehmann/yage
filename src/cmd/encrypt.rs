0312040204f5b41d2bb654f0ead8392fa7df6a28
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-02-12 17:33:34 +0100

    remove custom comparison
    now that age supports the comparison of the Recipients

--- src/cmd/encrypt.rs
+++ src/cmd/encrypt.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 use std::str::FromStr;
 
-use age::x25519;
 use serde_yaml as sy;
 
 use crate::cli::EncryptArgs;
@@ -19,7 +18,7 @@ pub fn encrypt(args: &EncryptArgs) -> Result<i32> {
         let yaml_recipients = get_yaml_recipients(&input_data)?;
         let recipients = if recipients.is_empty() {
             &yaml_recipients
-        } else if yaml_recipients.is_empty() || recipients_eq(&recipients, &yaml_recipients) {
+        } else if yaml_recipients.is_empty() || recipients == yaml_recipients {
             &recipients
         } else {
             return Err(YageError::InvalidRecipients);
@@ -29,20 +28,4 @@ pub fn encrypt(args: &EncryptArgs) -> Result<i32> {
         sy::to_writer(output, &output_data)?;
     }
     Ok(0)
-}
-
-// Recipient does not implement the required traits for a direct comparison of the vectors
-// so lets do it by hand
-fn recipients_eq(v1s: &[x25519::Recipient], v2s: &[x25519::Recipient]) -> bool {
-    if v1s.len() != v2s.len() {
-        return false;
-    }
-    let mut v1s: Vec<String> = v1s.iter().map(|r| r.to_string()).collect();
-    v1s.sort();
-    v1s.dedup();
-    let mut v2s: Vec<String> = v2s.iter().map(|r| r.to_string()).collect();
-    v2s.sort();
-    v2s.dedup();
-
-    v1s == v2s
 }
